0562133018d7631f89ff83490486d8302aca0095
Doug Enas <douglassenas@gmail.com>
2019-05-09 20:04:57 -0400

    registered models with site admin

--- project/urls.py
+++ project/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 from . import views
+from django.contrib import admin
 from rest_framework import routers
+
 
 router = routers.DefaultRouter()
 router.register('manufacturer', views.ManufacturerView)
 router.register('shoetype', views.ShoeTypeView)
 router.register('shoecolor', views.ShoeColorView)
 router.register('shoe', views.ShoeView)
+
+
 
 urlpatterns = [
     path('', include(router.urls))
--- shoe_api/urls.py
+++ shoe_api/urls.py
@@ -15,6 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from project.models import Manufacturer, ShoeType, ShoeColor, Shoe
+
+admin.site.register(Manufacturer)
+admin.site.register(ShoeType)
+admin.site.register(ShoeColor)
+admin.site.register(Shoe)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
